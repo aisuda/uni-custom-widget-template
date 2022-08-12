d8907df8906b3e75dedb3e3c547f8b2951e75450
wibetter <365533093@qq.com>
2022-08-12 17:01:56 +0800

    fix: 修复linkDebu失效

--- build/linkDebug.js
+++ build/linkDebug.js
@@ -3,6 +3,6 @@
  * 备注1: 需要先执行 npm run build:h5 生成「web端预览模块」（目录地址：preview/）
  * 备注2: 控制台生成的链接添加到爱速搭临时外链中
  */
-import '../preview/style.css';
-import '../preview/index.umd.js';
-import '../preview/plugin.umd.js';
+import '../web/renderer.css';
+import '../web/renderer.umd.js';
+import '../web/plugin.umd.js';
